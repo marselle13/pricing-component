e76c2a9091c138dc2d11265e537dbf4e16acb0ef
nika <nika.gogua8@gmail.com>
2023-01-16 22:43:56 +0400

    mobile design for pricing componenet

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const rangeInputs = document.querySelector('input[type="range"]');
+
+function handleInputChange(e) {
+  let target = e.target;
+  if (e.target.type !== "range") {
+    target = document.getElementById("range");
+  }
+  const min = target.min;
+  const max = target.max;
+  const val = target.value;
+
+  target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
+}
+
+rangeInputs.addEventListener("input", handleInputChange);
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,15 @@ module.exports = {
         "grayish-red": "hsl(14, 92%, 95%)", //discount bg
         "light-red": "hsl(15, 100%, 70%)", // dicsount text
         "pale-blue": "hsl(226, 100%, 87%)", // cta text
-        "Very Pale Blue": "hsl(230, 100%, 99%) ", //Main bg
-        "Light Grayish Blue ": "hsl(224, 65%, 95%)", //Empty Bar
-        "Light Grayish Blue": "hsl(223, 50%, 87%)", //Toggle Bg
-        "Grayish Blue": "hsl(225, 20%, 60%)", //Text
-        "Dark Desaturated Blue": "hsl(227, 35%, 25%)", //CTA Bg
+        "pale-white": "hsl(230, 100%, 99%) ", //Main bg
+        "grayish-white": "hsl(224, 65%, 95%)", //Empty Bar
+        "light-blue": "hsl(223, 50%, 87%)", //Toggle Bg
+        "grayish-blue": "hsl(225, 20%, 60%)", //Text
+        "desaturated-blue": "hsl(227, 35%, 25%)", //CTA Bg
+      },
+      backgroundImage: {
+        pattern: "url('../images/bg-pattern.svg')",
+        circle: "url('../images/pattern-circles.svg')",
       },
     },
   },
