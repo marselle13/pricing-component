31603327e54580dcd9f161c5151537276a9f52db
nika <nika.gogua8@gmail.com>
2023-01-17 00:47:38 +0400

    responsive pricing component logic

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
-const rangeInputs = document.querySelector('input[type="range"]');
+const rangeInput = document.querySelector('input[type="range"]');
+const pageViews = document.getElementById("pageviews");
+const price = document.getElementById("price");
+const discountInput = document.getElementById("discount");
+const views = ["10k", "50k", "100k", "500k", "1M"];
+const prices = [8, 12, 16, 24, 36];
 
 class App {
   constructor() {
-    rangeInputs.addEventListener("input", this._handleInputChange);
+    this.val = 2;
+    rangeInput.addEventListener("input", this._handleInputChange.bind(this));
+    discountInput.addEventListener("change", this._handleDiscount.bind(this));
   }
 
   _handleInputChange(e) {
@@ -12,10 +19,20 @@ class App {
     }
     const min = target.min;
     const max = target.max;
-    const val = target.value;
+    this.val = target.value;
 
-    target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
+    target.style.backgroundSize =
+      ((this.val - min) * 100) / (max - min) + "% 100%";
+    pageViews.textContent = `${views[this.val]} PAGEVIEWS`;
+    this._handleDiscount();
+  }
+
+  _handleDiscount() {
+    if (!discountInput.checked) {
+      price.textContent = `$${prices[this.val]}.00`;
+    } else {
+      price.textContent = `$${prices[this.val] * 0.75}.00`;
+    }
   }
 }
-
 const app = new App();
