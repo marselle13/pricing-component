f30f228a2d123e80325814af47b59c96235693c3
nika <nika.gogua8@gmail.com>
2023-01-17 17:07:59 +0400

    refactor code

--- src/script.js
+++ src/script.js
@@ -2,12 +2,13 @@ const rangeInput = document.querySelector('input[type="range"]');
 const pageViews = document.getElementById("pageviews");
 const price = document.getElementById("price");
 const discountInput = document.getElementById("discount");
-const views = ["10k", "50k", "100k", "500k", "1M"];
-const prices = [8, 12, 16, 24, 36];
 
 class App {
+  #prices = [8, 12, 16, 24, 36];
+  #views = ["10k", "50k", "100k", "500k", "1M"];
   constructor() {
     this.val = 2;
+
     rangeInput.addEventListener("input", this._handleInputChange.bind(this));
     discountInput.addEventListener("change", this._handleDiscount.bind(this));
   }
@@ -23,14 +24,14 @@ class App {
 
     target.style.backgroundSize =
       ((this.val - min) * 100) / (max - min) + "% 100%";
-    pageViews.textContent = `${views[this.val]} PAGEVIEWS`;
+    pageViews.textContent = `${this.#views[this.val]} PAGEVIEWS`;
     this._handleDiscount();
   }
 
   _handleDiscount() {
     price.textContent = !discountInput.checked
-      ? `$${prices[this.val]}.00`
-      : `$${prices[this.val] * 0.75}.00`;
+      ? `$${this.#prices[this.val]}.00`
+      : `$${this.#prices[this.val] * 0.75}.00`;
   }
 }
 const app = new App();
